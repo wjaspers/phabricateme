e900f0708a4ef367b119288ac704a6a25c64da61
William Jaspers <willjaspers4@gmail.com>
2013-11-26 00:57:38 -0600

    Fix the domain not updating the oauth config link.
    Also, git-started's pretty printer kicked in.

--- extension/js/options.js
+++ extension/js/options.js
@@ -2,104 +2,136 @@
  * Functionality strictly for the extension's options page.
  */
 (function (ph, uri) {
-	'use strict';
+    'use strict';
 
-	document.addEventListener('DOMContentLoaded', function () {
-		ph.fetchPlugin('Options', function () {
-			ph.Settings.load(function () {
-				/**
-				 * Listen for changes to the domain.
-				 */
-				var domain = document.getElementById("domain");
-				domain.addEventListener('change', function () {
-					var uri = new Uri(this.value);
-					ph.Settings.domain = uri.toString();
-					this.value = ph.Settings.domain;
-				});
+    document.addEventListener('DOMContentLoaded', function () {
+        ph.fetchPlugin('Options', function () {
+            ph.Settings.load(function () {
+                /**
+                 * Listen for changes to the domain.
+                 */
+                var domain = document.getElementById("domain");
+                domain.addEventListener('change', function () {
+                    var links = [], uri = new Uri(this.value);
+                    ph.Settings.domain = uri.toString();
+                    this.value = ph.Settings.domain;
+                    updateLinks(uri);
+                });
 
-				if (ph.Settings.domain) {
-					domain.value = ph.Settings.domain;
-				}
+                if (ph.Settings.domain) {
+                    domain.value = ph.Settings.domain;
+                    updateLinks(domain.value);
+                }
 
-				/**
-				 * 
-				 */
-				ph.Options.fetchPlugin('Authorization', function (plugin) {
-					var authMe = document.getElementById('authorize');
-					authMe.addEventListener('click', function (event) {
-						ph.Authorization.authorize();
-						event.preventDefault();
-						return false;
-					});
-				});
-
-
-				/**
-				 *
-				 */
-				ph.Options.fetchPlugin('Popup', function (plugin) {
-				});
+                /**
+                 * 
+                 */
+                ph.Options.fetchPlugin('Authorization', function (plugin) {
+                    var authMe = document.getElementById('authorize');
+                    authMe.addEventListener('click', function (event) {
+                        ph.Authorization.authorize();
+                        event.preventDefault();
+                        return false;
+                    });
+                });
 
 
-				/**
-				 *
-				 */
-				ph.Options.fetchPlugin('Shortcuts', function(plugin) {
-					var list = ph.Shortcuts.getAllLinks();
-					plugin.initialize(list);
-					domain.addEventListener('change', function () {
-						var uri = new Uri(this.value);
-						plugin.updateLinks(uri);
-					});
-					
-					plugin.updateLinks(ph.Settings.domain);
-				});
+                /**
+                 *
+                 */
+                ph.Options.fetchPlugin('Popup', function (plugin) {
+                });
 
 
-				/**
-				 * Bind a handler to the "save" button.
-				 */
-				document.getElementById("save").addEventListener('click', function(event) {
-					ph.Settings.save(function() {
-						alert("Settings saved!");
-					});
-					event.preventDefault();
-					return false;
-				});
+                /**
+                 *
+                 */
+                ph.Options.fetchPlugin('Shortcuts', function(plugin) {
+                    var list = ph.Shortcuts.getAllLinks();
+                    plugin.initialize(list);
+                    domain.addEventListener('change', function () {
+                        var uri = new Uri(this.value);
+                        plugin.updateLinks(uri);
+                    });
+                    
+                    plugin.updateLinks(ph.Settings.domain);
+                });
 
 
-				/*
-				 * Allow the user to wipe extension data.
-				 */
-				document.getElementById("terminate").addEventListener('click', function(event) {
-					if (confirm('Are you absolutely sure you want to erase ALL settings for PhabricateMe?')) {
-						ph.Settings.clear(function () {	
-							alert('Settings cleared.');
-						});
-					}
-					event.preventDefault();
-					return false;
-				});
-			});
-		});
-	});
+                /**
+                 * Bind a handler to the "save" button.
+                 */
+                document.getElementById("save").addEventListener('click', function(event) {
+                    ph.Settings.save(function() {
+                        alert("Settings saved!");
+                    });
+                    event.preventDefault();
+                    return false;
+                });
 
-	/**
-	 * Helper for controlling element visibility.
-	 *
-	 * @param DOMNode element
-	 * @param boolean state
-	 */
-	window.toggleVisibility = function (element, state) {
-		var classNames = element.className.split(' ');
-		classNames = classNames.filter(function (className) {
-			return className !== 'hide';
-		});
 
-		if (state === false) {
-			classNames.push('hide');
-		}
+                /*
+                 * Allow the user to wipe extension data.
+                 */
+                document.getElementById("terminate").addEventListener('click', function(event) {
+                    if (confirm('Are you absolutely sure you want to erase ALL settings for PhabricateMe?')) {
+                        ph.Settings.clear(function () {	
+                            alert('Settings cleared.');
+                        });
+                    }
+                    event.preventDefault();
+                    return false;
+                });
+            });
+        });
+    });
 
-		element.className = classNames.join(' ');
-	};
+    /**
+     * Helper for controlling element visibility.
+     * If I ever add a 3rd party js library this will probably go away.
+     *
+     * @param DOMNode element
+     * @param boolean state
+     */
+    window.toggleVisibility = function (element, state) {
+        var classNames = element.className.split(' ');
+        classNames = classNames.filter(function (className) {
+            return className !== 'hide';
+        });
+
+        if (state === false) {
+            classNames.push('hide');
+        }
+
+        element.className = classNames.join(' ');
+    };
+
+    window.updateLink = function updateLink (link, uri) {
+        if ('string' === typeof uri) {
+            uri = new Uri(uri);
+        }
+
+        link.protocol = uri.protocol;
+        if (uri.port) {
+            link.port = uri.port;
+        } else {
+            // FIXME: This is a bug in chrome
+            delete link.port;
+        }
+
+        link.hostname = uri.hostname;
+        link.pathname = Uri.sanitizePath(uri.pathname + link.pathname);
+    };
+
+    window.updateLinks = function updateLinks (uri) {
+        var links;
+        if ('string' === typeof uri) {
+            uri = new Uri(uri);
+        }
+
+        links = document.getElementsByClassName('preview');
+        Object.keys(links).forEach(function (link) {
+            window.updateLink(links[link], uri);	
+        });
+    };
 })(window.PhabricateMe, window.Uri);
--- extension/js/popup.js
+++ extension/js/popup.js
@@ -5,50 +5,50 @@
  * Wait for the popup's DOM to be ready.
  */
 (function(ph, uri) {
-	'use strict';
-	document.addEventListener('DOMContentLoaded', function () {
-		ph.fetchPlugin('Settings', function (plugin) {
-			plugin.initialize(function (settings) {
-				var shortcuts = settings.Shortcuts;
-				ph.fetchPlugin('Popup', function () {
-					ph.fetchPlugin('Shortcuts', function (plugin) {
-						var domain = ph.Settings.domain, uri;
-						uri = new Uri(domain);
-						makeLinks(plugin.getAllLinks(), shortcuts, uri);
-					});
-				});
+    'use strict';
+    document.addEventListener('DOMContentLoaded', function () {
+        ph.fetchPlugin('Settings', function (plugin) {
+            plugin.initialize(function (settings) {
+                var shortcuts = settings.Shortcuts;
+                ph.fetchPlugin('Popup', function () {
+                    ph.fetchPlugin('Shortcuts', function (plugin) {
+                        var domain = ph.Settings.domain, uri;
+                        uri = new Uri(domain);
+                        makeLinks(plugin.getAllLinks(), shortcuts, uri);
+                    });
+                });
 
-			});
-		});
-	});
+            });
+        });
+    });
 
-	function makeLinks(shortcutsList, shortcuts, uri) {
-		if ('string' === typeof uri) {
-			uri = new Uri(uri);
-		}
-		var links = document.getElementsByClassName('shortcut-preview');
-		Object.keys(shortcuts).forEach(function (name) {
-			var link, linkText;
-			if ('undefined' !== typeof shortcutsList[name]) {
-				linkText = document.createTextNode(shortcutsList[name].title);
-				link = document.createElement('a');
-				link.className = 'shortcut';
-				link.target = "_blank";
-				link.rel = name;
-				link.appendChild(linkText);
-				link.protocol = uri.protocol;
-				if (uri.port) {
-					link.port = uri.port;
-				} else {
-					// FIXME: This is a bug in chrome
-					delete link.port;
-				}
-				link.title = shortcutsList[name].tip;
-				link.hostname = uri.hostname;
-				link.pathname = Uri.sanitizePath(uri.pathname + shortcutsList[name].href);
-				document.body.appendChild(link);
-			}
-		});
-	};
+    function makeLinks(shortcutsList, shortcuts, uri) {
+        if ('string' === typeof uri) {
+            uri = new Uri(uri);
+        }
+        var links = document.getElementsByClassName('shortcut-preview');
+        Object.keys(shortcuts).forEach(function (name) {
+            var link, linkText;
+            if ('undefined' !== typeof shortcutsList[name]) {
+                linkText = document.createTextNode(shortcutsList[name].title);
+                link = document.createElement('a');
+                link.className = 'shortcut';
+                link.target = "_blank";
+                link.rel = name;
+                link.appendChild(linkText);
+                link.protocol = uri.protocol;
+                if (uri.port) {
+                    link.port = uri.port;
+                } else {
+                    // FIXME: This is a bug in chrome
+                    delete link.port;
+                }
+                link.title = shortcutsList[name].tip;
+                link.hostname = uri.hostname;
+                link.pathname = Uri.sanitizePath(uri.pathname + shortcutsList[name].href);
+                document.body.appendChild(link);
+            }
+        });
+    };
 
 })(window.PhabricateMe, window.Uri);
