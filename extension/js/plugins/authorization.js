cb5683fd0020c57a4681e757ca48f00309cd4d79
William Jaspers <willjaspers4@gmail.com>
2013-11-27 00:01:18 -0600

    Pass the domain into the oauth adapter.
    This will now use our modified version of the OAuth2-Extensions repo.
    - Pretty printer kicked in again..

--- extension/js/plugins/authorization.js
+++ extension/js/plugins/authorization.js
@@ -1,66 +1,68 @@
 (function (ph) {
-	'use strict';
+    'use strict';
 
-	function Authorization() {
-		this.adapter = null;
-	};
-
-
-	/**
-	 * Attempts to make an OAuth2 authorization against your Phabricator instance.
-	 * @param Closure callback
-	 */
-	Authorization.prototype.authorize = function (callback) {
-		var self = this;
-		this.initialize(function () {
-			self.adapter.authorize(function () {
-				var accessToken = self.adapter.getAccessToken();
-				if (callback) {
-					callback.apply(self);
-				}
-			});
-		});
-	};
+    function Authorization() {
+        this.adapter = null;
+    };
 
 
-	/**
-	 * Checks if we have an access token.
-	 * @return boolean
-	 */
-	Authorization.prototype.isAuthorized = function () {
-		return this.adapter.hasAccessToken();
-	};
+    /**
+     * Attempts to make an OAuth2 authorization against your Phabricator instance.
+     * @param Closure callback
+     */
+    Authorization.prototype.authorize = function (callback) {
+        var self = this;
+        this.initialize(function () {
+            self.adapter.authorize(function () {
+                var accessToken = self.adapter.getAccessToken();
+                if (callback) {
+                    callback.apply(self);
+                }
+            });
+        });
+    };
 
 
-	Authorization.prototype.getAccessToken = function () {
-		if (this.isAuthorized()) {
-			this.adapter.getAccessToken();
-		}
-
-		return undefined;
-	};
-
-	/**
-	 * Instantiates a new OAuth2 adapter.
-	 */
-	Authorization.prototype.initialize = function (callback) {
-		var self = this;
-		ph.Settings.load(function (settings) {
-			var domain = settings.domain, configuration = settings.Authorization;
-			configuration = {
-				'client_id': configuration['client_id'],
-				'client_secret': configuration['client_secret'],
-				'client_name': configuration['client_name'],
-				'domain': domain,
-				'api_scope': 'offline_access'
-			};
-			self.adapter = new OAuth2('phabricator', configuration);
-			if (callback) {
-				callback.apply(self);
-			}
-		});
-	};
+    /**
+     * Checks if we have an access token.
+     * @return boolean
+     */
+    Authorization.prototype.isAuthorized = function () {
+        return this.adapter.hasAccessToken();
+    };
 
 
-	ph.Authorization = new Authorization;
+    Authorization.prototype.getAccessToken = function () {
+        if (this.isAuthorized()) {
+            this.adapter.getAccessToken();
+        }
+
+        return undefined;
+    };
+
+    /**
+     * Instantiates a new OAuth2 adapter.
+     */
+    Authorization.prototype.initialize = function (callback) {
+        var self = this;
+        ph.Settings.load(function (settings) {
+            var domain = settings.domain, configuration = settings.Authorization;
+            configuration = {
+                'client_id': configuration['client_id'],
+                'client_secret': configuration['client_secret'],
+                'client_name': configuration['client_name'],
+                'api_scope': 'offline_access',
+                'options': {
+                    'domain': domain
+                }
+            };
+            self.adapter = new OAuth2('phabricator', configuration);
+            if (callback) {
+                callback.apply(self);
+            }
+        });
+    };
+
+
+    ph.Authorization = new Authorization;
 })(window.PhabricateMe);
